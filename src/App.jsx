3e31063f5595ef9b98fe540640fb7f18010616fd
alisonrobichaux <alisonrobichaux@gmail.com>
2024-02-21 13:54:46 -0600

    selected contact view

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
+
+
+    export default function App() {
+      const [SelectedContactId, setSelectedContactId] = useState(null);
+
+      return (
+        <>
+        {SelectedContactId ? (
+          <div>SelectedContactView</div>
+        ) : (
+          <ContactList />
+        )}
+        </>
+      );
+    }
+  
